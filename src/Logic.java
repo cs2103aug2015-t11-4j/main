e8e57a429a296ab9fec67eb8f8b176328b860a05
Jiahuan0102 <fan.jiahuan@gmail.com>
2015-10-08 01:45:24 +0800

    logic changed to follow parser and storage

--- src/Logic.java
+++ src/Logic.java
@@ -2,27 +2,70 @@ import java.util.ArrayList;
 
 public class Logic {
 	public static ArrayList<String> passToParser(String command){
-		ArrayList<String> commandAfterParser = Parser.analyzeCommand(command);
+		ArrayList<String> commandAfterParser = Parser.retrieveCommand(command);
 		return commandAfterParser;
 	}
 
 	public static void takeAction(ArrayList<String> commandAfterParser, ArrayList<String> contentList) {
 		int code = -1;
-		switch (commandAfterParser.get(0)){
-		case "Add":
-			String content = addOneItem(commandAfterParser, contentList);
-			code = Storage.modify(commandAfterParser.get(0), content);
+		switch (commandAfterParser.get(0).toLowerCase()){
+		case "add":
+			String content = createContentForAdd(commandAfterParser, contentList);
+			code = Storage.addOneItem(content);
 			break;
+		case "update":
+			content = createContentForUpdate(commandAfterParser, contentList);
+			code = Storage.updateOneItem(commandAfterParser.get(1), content); //pass in item number
+			break;
+		case "delete":
+			Storage.deleteOneItem(commandAfterParser.get(1));//pass in item number
+		case "display":
+			Storage.display();
+			
 		}
 		UI.feedback(commandAfterParser.get(0),code);
 	}
 
 
-	private static String addOneItem(ArrayList<String> commandAfterParser, ArrayList<String> contentList) {
-		String content = commandAfterParser.get(3)+" at "+commandAfterParser.get(2)+": "+commandAfterParser.get(1);
+
+
+	private static String createContentForUpdate(ArrayList<String> commandAfterParser, ArrayList<String> contentList) {
+		
+		return createContentForAdd(commandAfterParser, contentList);
+	}
+
+	private static String createContentForAdd(ArrayList<String> commandAfterParser, ArrayList<String> contentList) {
+		Task task = Parser.createTask(commandAfterParser);
+		String content = null;
+		switch (task.getTaskType().toLowerCase()){
+		case "deadline":
+			content = createContentForDeadline(task);
+			break;
+		case "event":
+			content = createContentForEvent(task);
+			break;
+		case "floating":
+			content = createContentForfloating(task);
+			break;
+		}
 		contentList.add(content);
 		return content;
 		
+	}
+
+	private static String createContentForfloating(Task task) {
+		
+		return task.getTaskDescription();
+	}
+
+	private static String createContentForEvent(Task task) {
+		
+		return task.getStartTime()+" - "+task.getEndTime()+ " on "+ task.getDate()+": "+task.getTaskDescription();
+	}
+
+	private static String createContentForDeadline(Task task) {
+
+		return "By "+task.getDate()+": "+ task.getTaskDescription();
 	}
 	
 
