9bf1f029fb638c2d5912d45bc2423838a1812db6
yujulucy <yuju888@hotmail.com>
2015-10-07 23:55:14 +0800

    new changes

---
+++ Project Skeleton/src/main/java/gui/CommandBarController.java
@@ -0,0 +1,56 @@
+package main.java.gui;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * This class handles the TextField that users input commands in and the Label
+ * that shows feedback.
+ * 
+ * @author Yu Ju
+ *
+ */
+public class CommandBarController extends BorderPane {
+
+    @FXML
+    private Label feedback;
+
+    @FXML
+    private TextField commandBar;
+
+    private static final String COMMAND_BAR_LAYOUT_FXML = "/main/resources/layouts/CommandBar.fxml";
+
+    private MainApp mainApp;
+
+    public CommandBarController(MainApp mainApp) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(COMMAND_BAR_LAYOUT_FXML));
+        loader.setController(this);
+        loader.setRoot(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.mainApp = mainApp;
+    }
+
+    @FXML
+    public void onKeyPress(KeyEvent event) {
+        mainApp.handleKeyPress(this, event.getCode(), commandBar.getText());
+    }
+
+    public void clear() {
+        commandBar.clear();
+    }
+
+    public void setFeedback(String feedbackText) {
+        feedback.setText(feedbackText);
+    }
+}
