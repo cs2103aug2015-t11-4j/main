95f047fa6dd76b144a5709948c2fc657092bb2a2
Lim Yong Zhi <limyz@u.nus.edu>
2015-10-07 16:49:03 +0800

    Added addToFile method in Storage

--- Project Skeleton/src/Storage.java
+++ Project Skeleton/src/Storage.java
@@ -1,8 +1,19 @@
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.PrintWriter;
 //import java.util.ArrayList;
 
+/*
+ * @@author Lim Yong Zhi
+ */
+
 public class Storage {
+	private static final String MESSAGE_ADD = "added to %s: \"%s\"";
+	private static final String MESSAGE_WRITE_ERROR = "unable to write file. check if read-only?";
+
+	String filename = "My Calender.txt";
+	
 /*	public static ArrayList<String> initializeFile(){
 		try {
 			PrintWriter outputStream = new PrintWriter("My Calender.txt");
@@ -30,5 +41,21 @@ public class Storage {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}	
+	}
+	
+	//adds a particular line to the file
+	private static void addToFile(String filename, String input) {
+		try {
+		FileWriter fw = new FileWriter(filename, true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		bw.write(input);
+		bw.newLine();
+		bw.close();
+		
+		System.out.println(String.format(MESSAGE_ADD, filename, input));
+		} catch (Exception e) {
+			System.out.println(MESSAGE_WRITE_ERROR);
+		}
 	}
 }
