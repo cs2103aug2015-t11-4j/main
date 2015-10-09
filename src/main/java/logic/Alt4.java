74cfa3c75539a83ec509e4dabcf29a7375ed918d
Lim Yong Zhi <limyz@u.nus.edu>
2015-10-09 11:32:15 +0800

    Separated into different java packages and updated Storage.java

---
+++ src/main/java/logic/Alt4.java
@@ -0,0 +1,20 @@
+package main.java.logic;
+import java.util.ArrayList;
+
+import main.java.resources.Task;
+import main.java.ui.UI;
+
+//@@Author: Jiahuan
+
+public class Alt4 {
+	public static ArrayList<Task> taskList;
+	public static void main(String[] args) {
+//		ArrayList<String> contentList = Storage.initializeFile();
+		ArrayList<String> contentList = new ArrayList<String>();
+		UI.welcome();
+		String command = UI.promoteCommand();
+		ArrayList<String> commandAfterParser = Logic.passToParser(command);
+		Logic.takeAction(commandAfterParser, contentList);
+	}
+
+}
---
+++ src/main/java/storage/Storage.java
@@ -0,0 +1,99 @@
+package main.java.storage;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import main.java.resources.Task;
+
+/**
+ * This class handles the Storage component and executes the 
+ * appropriate action requested by the Logic component
+ * 
+ * @@author Lim Yong Zhi
+ */
+
+public class Storage {
+	/* 
+	 * Temporary placeholder for creation of file - To be replaced with initializeFile
+	 */
+	private static String filename = "MyCalender.txt";
+	
+	public static int addOneItem(Task task) {
+		try {
+			FileWriter fw = new FileWriter(filename, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			bw.write(task.getTaskDescription());
+			bw.newLine();
+			bw.close();
+		} catch (Exception e) {
+			return -1;
+		}
+		return 0;
+	}
+	
+	public static int updateOneItem(int itemNumber, Task task) {
+		try {
+			FileWriter fw = new FileWriter(filename, true);
+			FileReader fr = new FileReader(filename);
+			
+			BufferedWriter bw = new BufferedWriter(fw);
+			BufferedReader br = new BufferedReader(fr);
+			
+			int lineNumber = 0;
+						
+			while ((br.readLine()) != null) {
+				lineNumber += 1;
+				if (lineNumber == itemNumber) {
+					bw.write(task.getTaskDescription());
+					bw.newLine();
+				}
+			}
+			
+			br.close();
+			bw.close();
+		} catch (Exception e) {
+			return -1;
+		}
+		return 0;
+	}
+	
+	public static int deleteOneItem(int itemNumber) {
+		try {
+			File original = new File(filename);
+			FileReader fr = new FileReader(filename);
+			BufferedReader br = new BufferedReader(fr);
+			
+			File temp = new File("Alt4.tmp");
+			FileWriter fw = new FileWriter("Alt4.tmp", true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			int lineNumber = 0;
+			String line = "";
+			
+			while ((line = br.readLine()) != null) {
+				lineNumber += 1;
+				if (lineNumber == itemNumber) {
+					bw.write(line);
+					bw.newLine();
+				}
+			}
+			
+			br.close();
+			bw.close();
+			original.delete();
+			temp.renameTo(original);
+			
+		} catch (Exception e) {
+			return -1;
+		}
+		return 0;
+	}
+
+	public static String getTaskTypeByItemNum(int itemNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
