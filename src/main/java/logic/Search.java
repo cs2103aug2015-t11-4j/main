2aa74f572d3e6a25b0e7cee3c2096e4e7ac71484
Lim Yong Zhi <limyz@u.nus.edu>
2015-10-16 12:20:17 +0800

    Search.java stub methods

---
+++ src/main/java/logic/Search.java
@@ -0,0 +1,44 @@
+package main.java.logic;
+
+import java.util.ArrayList;
+
+/**
+ * This class handles all search operations and executes the 
+ * appropriate action as requested by the Logic component
+ * 
+ * @@author Lim Yong Zhi
+ */
+
+public class Search {
+    public ArrayList<String> obtainTodaySummary(ArrayList<String> Tasks) {
+        return null;
+    }
+    
+    public ArrayList<String> obtainTommorrowSummary(ArrayList<String> Tasks) {
+        return null;
+    }
+    
+    public ArrayList<String> obtainAllTasks(ArrayList<String> Tasks) {
+        return null;
+    }
+    
+    public ArrayList<String> obtainAllIncompleteTasks(ArrayList<String> Tasks) {
+        return null;
+    }
+    
+    public ArrayList<String> obtainAllCompleteTasks(ArrayList<String> Tasks) {
+        return null;
+    }
+    
+    public ArrayList<String> obtainDeadlineTasks(ArrayList<String> Tasks) {
+        return null;
+    }
+    
+    public ArrayList<String> obtainEventTasks(ArrayList<String> Tasks) {
+        return null;
+    }
+    
+    public ArrayList<String> obtainFloatingTasks(ArrayList<String> Tasks) {
+        return null;
+    }
+}
