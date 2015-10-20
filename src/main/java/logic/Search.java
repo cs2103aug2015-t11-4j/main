ababbeeb5c089ad04103f62173ece72ed377a4c6
Lim Yong Zhi <limyz@u.nus.edu>
2015-10-20 16:36:55 +0800

    Completed Search methods (Search.java) for Logic component

--- src/main/java/logic/Search.java
+++ src/main/java/logic/Search.java
@@ -1,5 +1,6 @@
 package main.java.logic;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import main.java.resources.Task;
@@ -14,54 +15,53 @@ import main.java.storage.Storage;
 
 public class Search {
     /*
+     * Instantiate LocalDateTime to user's computer time
+     */
+    LocalDateTime ldt = LocalDateTime.now();
+    
+    /*
      * Obtains 10 deadlines, 10 events and 5 floating tasks to be seen in
      * the user's day as a summary
      * 
-     * TODO: Verfiy method is working
+     * TODO: Verify method is working
      * PREREQUISITE: Storage.taskList must be sorted by date and time
      */
     public ArrayList<Task> obtainTodaySummary() {
         ArrayList<Task> todaySummary = new ArrayList<Task>();
+        ArrayList<Task> taskList = Storage.taskList;
         
-        if(Storage.taskList.size() >= 25) {
-            for(int i = 0; i<10; i++) {
-                if(Storage.taskList.get(i).getTaskType().equals("deadline")) {
-                    todaySummary.add(Storage.taskList.get(i));
+        while(todaySummary.size() == 25 || taskList.isEmpty()) {
+            for(int i = 0; i<25; i++) {
+                if((taskList.get(i).getTaskType().equals("deadline"))) { 
+                    todaySummary.add(taskList.get(i));
+                    taskList.remove(i);
                 }
-            }
-        
-            for(int i = 0; i<10; i++) {
-                if(Storage.taskList.get(i).getTaskType().equals("event")) {
-                    todaySummary.add(Storage.taskList.get(i));
+
+                if(taskList.get(i).getTaskType().equals("event")) {
+                    todaySummary.add(taskList.get(i));
+                    taskList.remove(i);
                 }
-            }
-        
-            for(int i = 0; i<5; i++) {
-                if(Storage.taskList.get(i).getTaskType().equals("floating")) {
-                    todaySummary.add(Storage.taskList.get(i));
-                }
-            }
-        } else {
-            
-            for(int i = 0; i<Storage.taskList.size(); i++) {
-                if(Storage.taskList.get(i).getTaskType().equals("deadline")) {
-                    todaySummary.add(Storage.taskList.get(i));
-                }
-            }
-        
-            for(int i = 0; i<Storage.taskList.size(); i++) {
-                if(Storage.taskList.get(i).getTaskType().equals("event")) {
-                    todaySummary.add(Storage.taskList.get(i));
-                }
-            }
-        
-            for(int i = 0; i<Storage.taskList.size(); i++) {
-                if(Storage.taskList.get(i).getTaskType().equals("floating")) {
-                    todaySummary.add(Storage.taskList.get(i));
+
+                if(taskList.get(i).getTaskType().equals("floating")) {
+                    todaySummary.add(taskList.get(i));
+                    taskList.remove(i);
                 }
             }
         }
+        Storage.sortTaskList(todaySummary);
         return todaySummary;
+    }
+    
+    /*
+     * Return the corrected month format to compare with date stored in taskList
+     * Example: 01 (January) is returned instead of 1
+     */
+    private String toMonthValue() {
+        if((ldt.getMonthValue() == 1) || (ldt.getMonthValue() == 2) || (ldt.getMonthValue() == 3) || (ldt.getMonthValue() == 4) || (ldt.getMonthValue() == 5) ||
+                (ldt.getMonthValue() == 6) || (ldt.getMonthValue() == 7) || (ldt.getMonthValue() == 8) || (ldt.getMonthValue() == 9)) {
+            return "0" + ldt.getMonthValue();
+        }
+        return "" + ldt.getMonthValue();
     }
     
     /*
@@ -79,11 +79,10 @@ public class Search {
      * Obtains all tasks in the taskList
      */
     public ArrayList<Task> obtainAllTasks() {
-        try {
+        if(!Storage.taskList.isEmpty()) {
             return Storage.taskList;
-        } catch (Exception e) {
-            return null;
         }
+        return null;
     }
     
     /*
@@ -117,13 +116,31 @@ public class Search {
     }
     
     /*
-     * Obtains all tasks approaching deadlines (3 days) in the taskList
+     * Obtains all deadline tasks in the taskList
      */
     public ArrayList<Task> obtainDeadlineTasks() {
         ArrayList<Task> deadlines = new ArrayList<Task>();
         
         for(int i = 0; i<Storage.taskList.size(); i++) {
             if(Storage.taskList.get(i).getTaskType().equals("deadline")) {
+                deadlines.add(Storage.taskList.get(i));
+            }
+        }
+        
+        return deadlines;
+    }
+    
+    /*
+     * Obtains all tasks approaching deadlines (3 days) in the taskList
+     */
+    public ArrayList<Task> obtainApproachingDeadlineTasks() {
+        ArrayList<Task> deadlines = new ArrayList<Task>();
+        
+        for(int i = 0; i<Storage.taskList.size(); i++) {
+            if(Storage.taskList.get(i).getTaskType().equals("deadline")
+                    && (Storage.taskList.get(i).getEndDate().equals(ldt.getDayOfMonth()  + "/" + toMonthValue() + "/" + ldt.getYear()))
+                    && (Storage.taskList.get(i).getEndDate().equals((ldt.getDayOfMonth() + 1) + "/" + toMonthValue() + "/" + ldt.getYear()))
+                    && (Storage.taskList.get(i).getEndDate().equals((ldt.getDayOfMonth() + 2) + "/" + toMonthValue() + "/" + ldt.getYear()))) {
                 deadlines.add(Storage.taskList.get(i));
             }
         }
@@ -169,7 +186,11 @@ public class Search {
         ArrayList<Task> results = new ArrayList<Task>();
         
         for(int i = 0; i<Storage.taskList.size(); i++) {
-            if(Storage.taskList.get(i).getTaskDescription().contains(keyword)) {
+            if(Storage.taskList.get(i).getEndDate().contains(keyword)
+                    || Storage.taskList.get(i).getEndTime().contains(keyword)
+                    || Storage.taskList.get(i).getStartDate().contains(keyword)
+                    || Storage.taskList.get(i).getStartTime().contains(keyword)
+                    || Storage.taskList.get(i).getTaskDescription().contains(keyword)) {
                 results.add(Storage.taskList.get(i));
             }
         }
