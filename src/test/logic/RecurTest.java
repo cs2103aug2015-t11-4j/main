5406d37df43b24dbe4e20c7499613aed2e6cb4c4
Jiahuan0102 <fan.jiahuan@gmail.com>
2015-11-09 03:31:58 +0800

    test update

--- src/test/logic/RecurTest.java
+++ src/test/logic/RecurTest.java
@@ -1,12 +1,12 @@
-//@@author Jiahuan
 package test.logic;
 
-import java.util.ArrayList;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 import main.java.resources.Task;
 
-public class SortTest {
-
+public class RecurTest {
 	public static final String TYPE_DEADLINE = "deadline";
 	public static final String TYPE_EVENT = "event";
 	public static final String TYPE_FLOATING = "floating";
@@ -17,21 +17,12 @@ public class SortTest {
 	Task task4 = new Task(TYPE_EVENT, "wash hand with soap", "03/02/2015", "03/02/2015", "0915", "1100", true, true, 0);
 	Task task5 = new Task(TYPE_FLOATING, "meet with bob", "-", "-", "-", "-", false, true, 0);
 	Task task6 = new Task(TYPE_FLOATING, "eat breakfast", "-", "-", "-", "-", true, true, 0);
-	Task task7 = new Task(TYPE_DEADLINE, "wake up", "-", "01/01/2015", "-", "0900", false, true, 0);	
+	Task task7 = new Task(TYPE_DEADLINE, "wake up", "-", "01/01/2015", "-", "0900", false, true, 0);
 	
-	ArrayList<Task> sortList = new ArrayList<Task>();
-	ArrayList<Task> sortResult = new ArrayList<Task>();
-	ArrayList<Task> expected = new ArrayList<Task>();
 	
-/*	@Test
+	@Test
 	public void test() {
-		expected.add(task3);
-		expected.add(task1);
-		expected.add(task2);
-		expected.add(task4);
-		sortResult = Sort.sortByTime(sortList);
-		
-		assertEquals(expected, sortResult);
-	}*/
+		fail("Not yet implemented");
+	}
 
 }
